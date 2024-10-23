5308da9d52cd9c36a7e22c19ff877f83e9ac63d4
acidtib <arubinofaux@gmail.com>
2024-10-23 00:25:33 -0600

    wip

--- magic_prep_dataset.py
+++ magic_prep_dataset.py
@@ -4,6 +4,16 @@ from datasets import Dataset, DatasetDict, Image, ClassLabel, Features
 
 DATA_PATH = '/media/acid/turtle/datasets/tcg_magic'
 
+OUTPUT_PATH = '/media/acid/turtle/huggingface/tcg-magic-cards/data'
+
+# Function to get image paths
+def get_image_paths(directory):
+    return glob.glob(DATA_PATH + "/data/" + directory + "/**/*.jpg", recursive=True)
+
+# Function to get labels
+def get_labels(data_path):
+    return [os.path.basename(os.path.dirname(path)) for path in data_path]
+
 # Function to create a dataset for a split
 def create_split_dataset(image_paths, labels):
     # Get unique labels
@@ -26,30 +36,34 @@ def create_split_dataset(image_paths, labels):
 
 # Gather image paths from train, test, and valid directories
 print("Gathering image paths...")
-train_paths = glob.glob(DATA_PATH + "/data/train/**/*.jpg", recursive=True)
-valid_paths = glob.glob(DATA_PATH + "/data/valid/**/*.jpg", recursive=True)
+train_paths = get_image_paths("train")
+test_paths = get_image_paths("test")
+valid_paths = get_image_paths("valid")
 
 # Create labels based on folder structure or filenames
 print("Creating labels...")
-train_labels = [os.path.basename(os.path.dirname(path)) for path in train_paths]
-valid_labels = [os.path.basename(os.path.dirname(path)) for path in valid_paths]
+train_labels = get_labels(train_paths)
+test_labels = get_labels(test_paths)
+valid_labels = get_labels(valid_paths)
 
 # Create dataset for each split
 print("Creating datasets...")
 train_dataset = create_split_dataset(train_paths, train_labels)
+test_dataset = create_split_dataset(test_paths, test_labels)
 valid_dataset = create_split_dataset(valid_paths, valid_labels)
 
 # Combine splits into a DatasetDict
 print("Combining dataset...")
 dataset_dict = DatasetDict({
     "train": train_dataset,
+    "test": test_dataset,
     "valid": valid_dataset
 })
 
 # save as Arrow locally
 print("Saving dataset...")
 dataset_dict.save_to_disk(
-    "/media/acid/turtle/huggingface/tcg-magic-cards/data",
+    OUTPUT_PATH,
     num_proc=os.cpu_count()
 )
 
